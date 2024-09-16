3ceeeec4ddfca50ee7e9a8896ed370efc0431c5a
Zakaria3074 <zaki3074@gmail.com>
2024-09-16 13:48:04 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+"""je suis gay"""
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
+    """g"""
+    if p<=1:
+        return False
+    if p==2:
+        return True
+    if p%2:
+        return False
+    for d in range(3,int(sqrt(p+1))):
+        if p%d==0:
+            return False
+    return True  
 #### Fonction principale
 
-
 def main():
+    """h"""
 
     # vos appels à la fonction secondaire ici
 
